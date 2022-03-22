60f0bae087ab093f915467cf60db38e732095387
cmanor <101116379+cmanor@users.noreply.github.com>
2022-03-22 19:51:19 -0400

    done

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ list.push("fruit loops");
 list.splice(5,1, "fair trade coffee");
 list.splice(3,2, "rice", "beans");
 let cart =[]
-
 function cartTransferLast(list)
 {
     let thing = list.pop();
     cart.push(thing);
 }
-
 function cartTransferFirst(list)
 {
     let thing2 = list.shift();
     cart.push(thing2);
 }
-
 cartTransferLast(list);
 cartTransferFirst(list);
-
+while(list.length >0)
+{
+    cartTransferLast(list);
+}
+cart.sort();
+cart.reverse();
 console.log(list);
 console.log(cart);
+console.log(cart.toString());
